a2110bada0e7c1b45c1bead2677a91489ed602f0
Fernando Ferreira <bse.fernando@gmail.com>
2014-01-02 22:53:54 -0200

    Correctness and the Loop Invariant

---
+++ correctness.cpp
@@ -0,0 +1,37 @@
+// https://www.hackerrank.com/challenges/correctness-invariant
+
+#include <cstdio>
+
+void insertionSort(int ar_size, int *ar) {
+    int i,j, value;
+
+    for(i = 1; i < ar_size; i++) {
+        value = ar[i];
+        j = i-1;
+
+        while(j >= 0 && value < ar[j]) {
+            ar[j+1] = ar[j];
+            j = j-1;
+        }
+
+        ar[j+1]=value;
+    }
+
+    for(j = 0; j < ar_size; j++) {
+        printf("%d", ar[j]);
+        printf(" ");
+    }
+}
+
+int main() {
+    int _ar_size, _ar[_ar_size], _ar_i;
+
+    scanf("%d", &_ar_size);
+
+    for(_ar_i = 0; _ar_i < _ar_size; _ar_i++)
+       scanf("%d", &_ar[_ar_i]);
+
+    insertionSort(_ar_size, _ar);
+
+    return 0;
+}
